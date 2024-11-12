100aadf9216d45b3719b466d032580645a55ce10
alex <nguyena@plu.edu>
2024-11-12 10:41:58 -0800

    Deleted files, created "homepage"
    -Only changed FrontEnd

---
+++ FrontEnd/my-app/src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Premier Picks v1</h1>
+      </header>
+      <main>
+        <p>This is just a test homepage, to get a hang of React! c:</p>
+        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
