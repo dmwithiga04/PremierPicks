c342b355104b5ac5bce1c8689708b6f019653775
Davine Mungai <dmwithiga04@gmail.com>
2024-11-13 17:39:10 -0800

    added simple db usage

--- DataBase/db_backup.js
+++ DataBase/db_backup.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 
+
 const con = mysql.createConnection({
   host: "localhost", // Use "localhost" if Node.js is outside Docker
   port: 3306,
@@ -9,6 +10,9 @@ const con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+  con.query("select  * from movies limit ?",[20], function(err, result, fields) {
+    if (err) throw err;
+    const data = result;
+    console.log(data);
+  });
 });
